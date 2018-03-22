06b13b8d303632a88201a6972b3df4030e58b5a2
niedongsheng <niedongsheng@doctorwork.com>
2018-03-22 15:34:51 +0800

    测试微信

---
+++ controller/healthController.js
@@ -0,0 +1,81 @@
+
+delete require.cache[require.resolve("../service/healthService")];
+let HealthService = require("../service/healthService");
+let healthService = new HealthService();
+
+module.exports = {
+    helloworld : function(req,res,next) {
+        res.send("helloworld");
+    },
+
+    createTable : function(req, res, next) {
+        healthService.createTable(function(result) {
+            res.send(result);
+        });
+    },
+
+    signUp : function(req, res, next) {
+        healthService.insertUser(req.body,function(result) {
+            res.send({success: true});
+        });
+    },
+
+    getCaptionCode : function(req, res, next) {
+        res.send({success: true});
+    },
+
+    listUser : function(req, res, next) {
+        healthService.listUser(req.body,function(result){
+            res.send(result);
+        });
+    },
+    insertUser : function(req, res, next) {
+        console.log(req.body);
+        healthService.insertUser(req.body,function(result){
+            res.send({success: true});
+        });
+    },
+    updateUser : function(req, res, next) {
+        console.log(req.body);
+        healthService.updateUser(req.body,function(result){
+            res.send({success: true});
+        });
+    },
+    deleteUser : function(req, res, next) {
+        healthService.deleteUser(req.body,function(result){
+            res.send({success: true});
+        });
+    },
+
+    listHealthProduct:  function(req, res, next) {
+        healthService.listHealthProduct(req.body,function(result){
+            res.send(result);
+        });
+    },
+
+    getHealthProduct : function(req, res, next) {
+        healthService.getHealthProduct(req.body,function(result){
+            res.send(result);
+        });
+    },
+
+
+    userInfo : function(req, res, next ) {
+        healthService.userInfo(req.body,function(result) {
+            res.send(result);
+        });
+    },
+
+    buyHealthProduct : function(req,res,next) {
+        healthService.buyHealthProduct(req.body,function(result) {
+            res.send(result);
+        });
+    },
+
+
+    wechat: function(req,res,next) {
+        res.send(req.query.echostr);
+    }
+
+
+}
