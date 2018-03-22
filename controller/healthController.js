53e74d67fd44d47128f7355e6f40a1fecc2e14b7
niedongsheng <niedongsheng@doctorwork.com>
2018-03-22 16:17:42 +0800

    微信

--- controller/healthController.js
+++ controller/healthController.js
@@ -74,7 +74,7 @@ module.exports = {
 
 
     wechat: function(req,res,next) {
-        console.log("推送"+JSON.stringify(req.query));
+        console.log(req);
         res.send(req.query.echostr);
     },
 
