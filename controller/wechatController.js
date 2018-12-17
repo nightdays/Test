496edd7f44af80c7795ef45acb8192260062a53f
niedongsheng <niedongsheng@doctorwork.com>
2018-12-17 12:51:57 +0800

    测试

--- controller/wechatController.js
+++ controller/wechatController.js
@@ -1,22 +1,35 @@
 function req(cb) {
-    return function(req,res,next) {
-        try{
-            cb(req,res,next);
-        }catch(error) {
-            res.send({
-                data : null,
-                errmsg : error,
-                code : 500
-            });
-        }
+  return function(req, res, next) {
+    try {
+      cb(req, res, next);
+    } catch (error) {
+      res.send({
+        data: null,
+        errmsg: error,
+        code: 500
+      });
     }
+  };
 }
 
 let baseName = "";
 
 module.exports = {
-    "weChat/queryOrderProgress" : req(function(req,res,next) {
-        res.send("helloworld");
-    }),
-
-}
+  "weChat/queryOrderProgress": req(function(req, res, next) {
+    let list = [
+      {
+        orderid: "18921312312323",
+        expectTime: "2018-01-01",
+        mgr: "贾斯汀比伯",
+        taskname: "施工环节"
+      },
+      {
+        orderid: "18921312312323",
+        expectTime: "2018-01-01",
+        mgr: "贾斯汀比伯",
+        taskname: "开通交付"
+      }
+    ];
+    res.send("helloworld");
+  })
+};
