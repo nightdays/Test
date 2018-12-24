942e82228168f35db4a430f35fd28027a2617ba4
niedongsheng <niedongsheng@doctorwork.com>
2018-12-24 16:20:32 +0800

    测试

--- controller/standardController.js
+++ controller/standardController.js
@@ -1,41 +1,60 @@
-let page = require('./page.js');
+let page = require("./page.js");
 function req(cb) {
-    return function(req, res, next) {
-      try {
-        cb(req, res, next);
-      } catch (error) {
-        res.send({
-          data: null,
-          errmsg: error,
-          code: 500
-        });
-      }
-    };
-  }
-  
-  let baseName = "";
-  
-  module.exports = {
-    "standard/queryCutoverList.do": req(function(req, res, next) {
-        let param = req.body;
-        console.log(param)
-        // console.log(123 , param); 
-        let p = page(param.pageNum,param.pageSize);
-        
-      let result = {
-        total : p.total
-      }
-      let arr = [];
-      for(let data of p.data) {
-        let temp = {};
-        temp.TITLE = data.index + '四川阿斯顿发射点开发了骄傲的放假啊三六九等阿斯顿发生发射点发啊手动阀手动阀阿斯蒂芬阿斯蒂芬阿斯蒂芬阿斯蒂芬啊';
-        temp.START_DATE = "2017-01-01";
-        temp.END_DATE = "2018-01-01";
-        temp.CITY = "无锡市";
-        arr.push(temp);
-      }
-      result.data = arr;
-      res.send(result);
-    })
+  return function(req, res, next) {
+    try {
+      cb(req, res, next);
+    } catch (error) {
+      res.send({
+        data: null,
+        errmsg: error,
+        code: 500
+      });
+    }
   };
-  
+}
+
+let baseName = "";
+
+module.exports = {
+  "standard/queryCutoverList.do": req(function(req, res, next) {
+    let param = req.body;
+    console.log(param);
+    let p = page(param.pageNum, param.pageSize);
+
+    let result = {
+      total: p.total
+    };
+    let arr = [];
+    for (let data of p.data) {
+      let temp = {};
+      temp.ID = data.index;
+      temp.TITLE =
+        data.index +
+        "四川阿斯顿发射点开发了骄傲的放假啊三六九等阿斯顿发生发射点发啊手动阀手动阀阿斯蒂芬阿斯蒂芬阿斯蒂芬阿斯蒂芬啊";
+      temp.START_DATE = "2017-01-01";
+      temp.END_DATE = "2018-01-01";
+      temp.CITY = "无锡市";
+      arr.push(temp);
+    }
+    result.data = arr;
+    res.send(result);
+  }),
+  "standard/queryCutoverDetailList.do": req(function(req, res, next) {
+    let param = req.body;
+    let p = page(1, param.pageSize);
+
+    let result = {
+      total: p.total
+    };
+    let arr = [];
+    for (let data of p.data) {
+      let temp = {};
+      temp.username = '四川王附近'+data.index;
+      temp.userno = '888888888';
+      temp.city = '成都';
+      arr.push(temp);
+    }
+    result.data = arr;
+    res.send(result);
+  })
+};
