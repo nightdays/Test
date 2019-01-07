6023f3c49b0a0f53855a7d3b7fa532dae23804bf
niedongsheng <niedongsheng@doctorwork.com>
2019-01-07 16:52:15 +0800

    hellowrold

--- controller.js
+++
@@ -1,9 +0,0 @@
-
-// delete require.cache[require.resolve("./controller/YogaController")];
-// let YogaController = require("./controller/YogaController");
-delete require.cache[require.resolve("./controller/standardController")];
-let controller = require("./controller/standardController");
-
-let baseControllers = Object.assign({},controller);
-
-module.exports = baseControllers;
--- controller/standardController.js
+++ controller/standardController.js
@@ -37,58 +37,8 @@ module.exports = {
       arr.push(temp);
     }
     result.data = arr;
-    res.send(result);
-  }),
-
-  "standard/queryB": req(function(req, res, next) {
-    let param = req.body;
-    let p = page(param.pageNum, param.pageSize);
-
-    let result = {
-      total: p.total
-    };
-    res.send(result);
-  }),
-
-  "standard/queryCutoverList.do": req(function(req, res, next) {
-    let param = req.body;
-    console.log(param);
-    let p = page(param.pageNum, param.pageSize);
-
-    let result = {
-      total: p.total
-    };
-    let arr = [];
-    for (let data of p.data) {
-      let temp = {};
-      temp.ID = data.index;
-      temp.TITLE =
-        data.index +
-        "四川阿斯顿发射点开发了骄傲的放假啊三六九等阿斯顿发生发射点发啊手动阀手动阀阿斯蒂芬阿斯蒂芬阿斯蒂芬阿斯蒂芬啊";
-      temp.START_DATE = "2017-01-01";
-      temp.END_DATE = "2018-01-01";
-      temp.CITY = "无锡市";
-      arr.push(temp);
-    }
-    result.data = arr;
-    res.send(result);
-  }),
-  "standard/queryCutoverDetailList.do": req(function(req, res, next) {
-    let param = req.body;
-    let p = page(1, param.pageSize);
-
-    let result = {
-      total: p.total
-    };
-    let arr = [];
-    for (let data of p.data) {
-      let temp = {};
-      temp.username = '四川王附近'+data.index;
-      temp.userno = '888888888';
-      temp.city = '成都';
-      arr.push(temp);
-    }
-    result.data = arr;
+    console.log(__dirname);
     res.send(result);
   })
+
 };
