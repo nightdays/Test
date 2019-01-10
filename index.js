8c74de786bd3d4db2de2e44e33d7f8dd4994d63e
niedongsheng <niedongsheng@doctorwork.com>
2019-01-10 15:10:52 +0800

    fasong

--- index.js
+++ index.js
@@ -2,5 +2,9 @@
 module.exports = {
     helloWorld: function(req , res) {
         res.send("hello ddddddf");
+    },
+    test: function(req,res) {
+        let d = req.data ;
+        res.send("你发送的pageNum是" + d.pageNum);
     }
 }
